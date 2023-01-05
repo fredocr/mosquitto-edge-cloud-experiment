27172928e81c35feb845b270c101f4499a3cdbe7
fredocr <feruilova@yahoo.com>
2023-01-05 18:22:04 +0100

    Added "send message "apple " to load tester"

--- run.py
+++ run.py
@@ -1,45 +1,75 @@
+# Import necessary modules
+import paho.mqtt.client as mqtt
+import random
 import time
-import subprocess
-import os
-import psutil
-import pyshark
 
-# Define local interface
-interface = 'any'
+#Initial counter 
+counter = 0
 
-# Generate output file names with timestamp
-timestamp = time.strftime("%Y%m%d-%H%M%S")
-output_pcap_file = "output.pcap" if not os.path.exists("output.pcap") else "output_{}.pcap".format(timestamp)
-output_stats_file = "performance_stats.txt" if not os.path.exists("performance_stats.txt") else "performance_stats_{}.txt".format(timestamp)
+# Set the broker address and port
+broker_address = "37.152.61.101"
+broker_port = 1883
 
-# Start pcap capture on local interface
-capture = pyshark.LiveCapture(interface=interface)
-capture.sniff(timeout=5)
+# Set the topics and number of devices
+topics = ["sensor1", "sensor2", "sensor3"]
+num_devices = 2000
 
-# Run external script "load-testv3.py"
-process = subprocess.Popen(["python", "load-testv3.py"])
+# Set the number of messages to publish per device
+num_messages = 200
 
-# Monitor performance of "load-testv3.py"
-cpu_usage = []
-memory_usage = []
+# Function to generate random data
+def generate_data():
+    data = ""
+    for i in range(10):
+        data += str(random.randint(0,9))
+    return data
+
+# Function to be called when a message is published
+def on_publish(client, userdata, mid):
+    global counter
+    counter += 1
+
+# Create a new MQTT client
+client = mqtt.Client()
+
+# Set the on_publish function to be called when a message is published
+client.on_publish = on_publish
+
+# Connect to the broker
+client.connect(broker_address, broker_port)
+
+# Set the time variable
+run_time = 1800 #10800 = 3 hours in seconds
+
+# Start a loop to publish messages from each device
 start_time = time.time()
-while process.poll() is None:
-    # Record CPU usage
-    cpu_usage.append(psutil.cpu_percent())
-    # Record memory usage
-    memory_usage.append(psutil.Process(process.pid).memory_info().rss)
-    time.sleep(0.1)
+while time.time() - start_time < run_time:
+    for i in range(num_devices):
+        for j in range(num_messages):
+            # Choose a random topic
+            topic = random.choice(topics)
 
-# Calculate average performance metrics
-elapsed_time = time.time() - start_time
-average_cpu_usage = sum(cpu_usage) / len(cpu_usage)
-average_memory_usage = sum(memory_usage) / len(memory_usage)
+            # Generate data for each device and topic
+            data = generate_data()
 
-# Write performance metrics to output file
-with open(output_stats_file, 'w') as f:
-    f.write("Average CPU usage: {}%\n".format(average_cpu_usage))
-    f.write("Average memory usage: {} bytes\n".format(average_memory_usage))
-    f.write("Average running time: {} seconds\n".format(elapsed_time))
+            # Publish the data to the broker
+            client.publish(topic, data)
 
-# Save pcap capture to file  not working at the moment 
-#capture.save_to_file(output_pcap_file)
+    # Sleep for 1 second before publishing again
+    time.sleep(1)
+
+
+# Sends the word apple, so server could parse it and abort sript.
+client.publish(topic, "apple")
+
+
+# Function to show a summary of the data transferred
+def show_summary():
+    data_transferred = counter * 10 # 10 bytes per message
+    data_transferred_mb = data_transferred / 1000000 # Convert to megabytes
+    print(f"Total data transferred: {data_transferred_mb} MB")
+    print(f"Total messages sent: {counter}")
+    print(f"Total time elapsed: {run_time} seconds")
+
+# Call the show_summary function
+show_summary()
